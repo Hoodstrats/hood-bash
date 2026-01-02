a54baee5d932bd79301bb95f871da650be4fdfc3
Hoodstrats <hoodstrats@gmail.com>
2026-01-01 21:00:18 -0500

    Fixed: steam helper now correctly deletes the game's install directory in the COMMON folder using the "installdir" entry from the game's ACF library file instead of the "name" entry which a lot of the time is different

--- hood_steam.sh
+++ hood_steam.sh
@@ -58,28 +58,33 @@ delete(){
   find "$ACF_DIR" -type f -name "*.acf" | while read -r acf_file; do
       appid=$(grep -m1 '"appid"' "$acf_file" | sed 's/.*"\([0-9]*\)".*/\1/')
       name=$(grep -m1 '"name"' "$acf_file" | sed 's/.*"\([^"]*\)".*/\1/')
+      install_loc=$(grep -m1 '"installdir"' "$acf_file" | sed 's/.*"\([^"]*\)".*/\1/')
       if [[ "${name,,}" == *"${del,,}"* ]]; then
             echo -e "\e[31mDeleting game: $name (AppID: $appid)\e[0m"
 
           # remove the dirs under compatdata and shadercache
-          for data_dir in "$ACF_DIR/$DATA_DIR" "$ACF_DIR/$SHADER_DIR" "$ACF_DIR/$INSTALL_DIR"; do
+          for data_dir in "$ACF_DIR/$DATA_DIR" "$ACF_DIR/$SHADER_DIR"; do
             if [ -d "$data_dir" ]; then
               if [ -e "$data_dir/$appid" ] || [ -L "$data_dir/$appid" ]; then
                 rm -rf "$data_dir/$appid"
                 echo -e "\e[31mRemoved $data_dir/$appid\e[0m"
               fi
-              if [ -e "$data_dir/$name" ] || [ -L "$data_dir/$name" ]; then
-                rm -rf "$data_dir/$name"
-                echo -e "\e[31mRemoved $data_dir/$name\e[0m"
-              fi
-              # since the user_data_dir has multiple folders 1 level down need to check for the appid in all of those
-              if [ -d "$USER_DATA_DIR" ]; then
-               find "$USER_DATA_DIR" -type d -name "$appid" -exec rm -rf {} + -print 2>/dev/null | while read -r removed_dir; do
-                  echo -e "\e[31mRemoved $removed_dir\e[0m"
-                done
-              fi
             fi
           done
+
+          # remove the actual game install directory using installdir entry in the acf file
+          install_path="$ACF_DIR/$INSTALL_DIR/$install_loc"
+          if [ -d "$install_path" ]; then
+            rm -rf "$install_path"
+            echo -e "\e[31mRemoved $install_path\e[0m"
+          fi
+
+          # since the user_data_dir has multiple folders 1 level down need to check for the appid in all of those
+          if [ -d "$USER_DATA_DIR" ]; then
+            find "$USER_DATA_DIR" -type d -name "$appid" -exec rm -rf {} + -print 2>/dev/null | while read -r removed_dir; do
+              echo -e "\e[31mRemoved $removed_dir\e[0m"
+            done
+          fi
           echo -e "\e[31mRemoved appid $appid from compatdata/shadercache and other library folders\e[0m"
       fi
   done
